caa06b859e990e294fc2dc87fffd21ff2ae03056
Bill <bill.kurtson@gmail.com>
2018-10-02 20:37:10 +0200

    add functionality for ignoring files or directories (#3)

--- fileCounter.js
+++ fileCounter.js
@@ -4,14 +4,33 @@ const dir = process.argv.slice(2, 3).pop();
 let scanSubdir = process.argv.slice(3, 4);
 let count = 0;
 let files;
+let ignoreFiles = [];
+
+
+function loadIgnoreFiles() {
+    if (process.argv.length < 5) {
+        return;
+    }
+
+    for (let i = 4; i < process.argv.length; ++i) {
+        ignoreFiles.push(process.argv[i]);
+    }
+}
 
 function getCount(files, parent) {
     let subFiles;
 
     try {
         files.forEach(file => {
-            const filePath = `${parent}/${file}`;
-            const fileStats = fs.statSync(filePath);
+            let filePath;
+            let fileStats;
+
+            if (ignoreFiles.includes(file)) {
+                return;
+            }
+
+            filePath = `${parent}/${file}`;
+            fileStats = fs.statSync(filePath);
 
             if (fileStats.isDirectory()) {
                 subFiles = fs.readdirSync(filePath);
@@ -30,19 +49,27 @@ function getCount(files, parent) {
 }
 
 function main() {
-    console.log(dir);
+    console.log(`Scanning ${dir}`);
+
+    if (scanSubdir) {
+        console.log('Including subdirectores');
+    } else {
+        console.log('Ignoring subdirectories');
+    }
 
     try {
         files = fs.readdirSync(dir);
     } catch (e) {
-        console.log(`couldn't read dir`);
+        console.log(`Couldn't read directory ${dir}`);
         console.log(e);
     }
+
+    loadIgnoreFiles();
 
     scanSubdir = scanSubdir || [];
     count = getCount(files, dir);
 
-    console.log(`number of files: ${count}`);
+    console.log(`Number of files: ${count}`);
 
     return count;
 }
